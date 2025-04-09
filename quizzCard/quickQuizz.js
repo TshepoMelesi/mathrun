a4ba1438e3b0784c4cfbc23aa889eab9b5a2e4d3
TshepoMelesi <twmelesi@gmail.com>
2025-04-09 02:03:06 +0200

    added new UI and features

--- quizzCard/quickQuizz.js
+++ quizzCard/quickQuizz.js
@@ -7,11 +7,52 @@ const cardAnswer = document.querySelector(".card-answer")
 const dashboard = document.querySelector(".dashboard")
 const quizzList = document.querySelector(".quizz-list")
 const dashboardContent = document.querySelector(".dashboard-content")
-const quizzFront = document.querySelector(".quizz-front")
-const quizzBack = document.querySelector(".quizz-back")
+const quizzFront = document.querySelector(".front-content")
+const quizzBack = document.querySelector(".back-content")
+const main = document.querySelector(".app-main")
+const addCardPage = document.querySelector(".app-add-card")
+const cardListPage = document.querySelector(".app-card-list")
+const playCardPage = document.querySelector(".app-play-cards")
+const footerActionBtns= document.querySelectorAll(".footer-action")
+const track = document.querySelector(".track")
 
-const app = {name : "asm-app", quizzes : [], mode : "CARD"}
 
+const modes = ["ADD_CARD", "CARD_LIST", "PLAY_CARDS", "HOME"]
+const pages = {
+    currentPage : playCardPage,
+    mode : "CARD",
+    pageList : {
+        "ADD_CARD": {
+            element : addCardPage,
+            "leftAction" : {func : () => handleDashboard(), name : "view cards"},
+            "rightAction" : {func : () => handleAddQuizz(), name : "save card"},
+        },
+        "CARD_LIST": {
+            element : cardListPage,
+            "leftAction" : {func : () => handleClearAll(), name : "clear all"},
+            "rightAction" : {func : () => routeTo("ADD_CARD"), name : "add card"},
+        },
+        "PLAY_CARDS": {
+            element : playCardPage,
+            "leftAction" : {func : () => handlePrev(), name : "previous"},
+            "rightAction" : {func : () => handleNext(), name : "next card"},
+        },
+    }
+}
+const app = {
+    name : "asm-app", 
+    quizzes : [], 
+    
+}
+
+const sanitizeHTML = (input) => {
+    const temp = document.createElement("div")
+    temp.textContent = input
+    return temp.innerHTML
+}
+const sanitizeText = (input) => {
+    return input.trim().replace(/[^a-zA-Z0-9 .,!?-]/g, "")
+}
 const generateId = (max = 10000000, min = 0) => {
     return Math.round(Math.random() * (max - min) + min)
 }
@@ -31,14 +72,14 @@ const saveQuizz = (quizz) => {
 
     if(!appDb) {
         createDb(app.name)
-        let appDb = JSON.parse(localStorage.getItem(app.name))
+        // let appDb = JSON.parse(localStorage.getItem(app.name))
     }
 
     if(appDb?.quizzes.find(q => 
         q.question == quizz.question && 
         q.answer == quizz.answer)
     ){
-        return //console.log("found a duplicate of", quizz)
+        return
     }
 
     appDb.quizzes.push(quizz)
@@ -91,20 +132,40 @@ const handlePrev = () => {
 }
 const handleFlip = () => {
     cardInner.style = "transform: rotateX(180deg) rotateY(0deg) rotateZ(180deg);"
+    if(timer) clearInterval(timer)
 }
 const handleNext = () => {
     cardInner.style = "transform: rotateX(0deg) rotateY(0deg) rotateZ(0deg);"
     if(!getDb().quizzes.length) return
     const quizz = generateQuizz()
     display(quizzes[quizzes.length - 1])
+    stopwatch()
 }
 const toggleMode = () =>{
-    if(app.mode == "DASHBOARD"){
-        app.mode = "HOME"
+    if(getMode() == "DASHBOARD"){
+        setMode("HOME")
     } else {
-        app.mode = "DASHBOARD"
+        setMode("DASHBOARD") 
     }
 }
+const setMode = (mode) => {
+    if(modes.indexOf(mode) > -1){
+        return pages.mode = mode
+    }
+
+    return console.log("Unknown Mode : ", mode)
+}
+const getMode = () => {
+    return pages.mode
+}
+
+const setCurrPage = (page) => {
+    return pages.currentPage = page.element
+}
+const getCurrPage = () => {
+    return pages.currentPage
+}
+
 const clearInputs = () => {
     quizzFront.value = ""
     quizzBack.value = ""
@@ -114,22 +175,21 @@ const populateInputs = (quizz) => {
     quizzBack.value = quizz.answer
 }
 const handleAddQuizz = () => {
-    const front = quizzFront.value
-    const back = quizzBack.value
-
+    const front = sanitizeText(sanitizeHTML(quizzFront.value))
+    const back = sanitizeText(sanitizeHTML(quizzBack.value))
+    
     if(!front || !back) return
+    console.log(front, back)
 
     addQuizz(front, back)
     db = getDb()
     refreshList()
     clearInputs()
 }
-
 const handleEdit = (id) => {
     const quizz = db.quizzes[getQuizzIndex(id)]
 
     populateInputs(quizz)
-    console.log(id, quizz)
     removeQuizz(id)
 }
 const handleDelete = (id) => {
@@ -154,38 +214,76 @@ const refreshList = () =>{
         quizzList.innerHTML += createItem(q)
     })
 }
+const handleClearAll = () => {
+    app.quizzes = []
 
+    localStorage.setItem(app.name, JSON.stringify(app))
+    refreshList()
+
+    return console.log("cleared your card list")
+}
 const handleDashboard = () => {
-    toggleMode()
-
-    if(app.mode == "DASHBOARD"){
-        dashboard.style.zIndex = 1000000
-        dashboard.style.display = "flex"
-
-        refreshList()
-    } else {
-        dashboard.style.display = "none"
-        dashboard.style.zIndex = -1000000
-    }
+    routeTo("CARD_LIST")
+    setMode("CARD_LIST")
 }
-
 const handleHome = () => {
-        app.mode = "HOME"
+        setMode("HOME")
         dashboard.style.display = "none"
         dashboard.style.zIndex = -1000000
 }
-
-refreshList()
-
 if(width > height){
     // 16/9
     dashboardContent.style.flexDirection = "row"
 } else{
     // 9/16
     dashboardContent.style = `
-        flex-direction : column;
-        gap: 2em;
-        align-items : center;
-        justify-content: start;
+    flex-direction : column;
+    gap: 2em;
+    align-items : center;
+    justify-content: start;
     `
 }
+const routeTo = (mode) => {
+    setMode(mode)
+
+    renderMain()
+    console.log(pages.mode)
+}
+const renderMain = () => {
+    getCurrPage().style.zIndex = "0"
+    getCurrPage().style.display = "none"
+    pages.pageList[getMode()].element.style.zIndex = "10"
+    pages.pageList[getMode()].element.style.display= "flex"
+    setCurrPage(pages.pageList[getMode()])
+
+    footerActionBtns[0].innerText = pages.pageList[getMode()].leftAction.name
+    footerActionBtns[1].innerText = pages.pageList[getMode()].rightAction.name
+}
+
+const handleLeftAction = () => {
+    pages.pageList[getMode()].leftAction.func()
+}
+
+const handleRightAction = () => {
+    pages.pageList[getMode()].rightAction.func()
+}
+
+let timer = null
+const stopwatch = () => {
+    clearInterval(timer)
+    let counter = 3
+    timer = setInterval(() => {
+        if(counter >=98){
+            counter = 3
+            clearInterval(timer)
+        }
+        counter += 1
+        track.style.width = counter + "%"
+    }, 1000 * 1.5)
+}
+
+routeTo("ADD_CARD")
+routeTo("CARD_LIST")
+routeTo("PLAY_CARDS")
+
+refreshList()
